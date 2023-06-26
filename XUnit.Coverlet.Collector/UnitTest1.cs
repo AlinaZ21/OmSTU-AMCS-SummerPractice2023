a4c457d22607bb0c0a19764b6a9233d8827a9a7a
AlinaZ <a.n.zadorojnaia@gmail.com>
2023-06-26 18:09:05 +0000

    0.1

--- XUnit.Coverlet.Collector/UnitTest1.cs
+++ XUnit.Coverlet.Collector/UnitTest1.cs
@@ -6,93 +6,79 @@ namespace UnitTest1;
 
 public class UnitTest1
 {
-[Theory]
-[InlineData(0, 1, 1)]
+    [Theory]
+    [InlineData(0, 1, 1)]
+    [InlineData(double.PositiveInfinity, 1, 1)]
+    [InlineData(1, double.PositiveInfinity, 1)]
+    [InlineData(1, 1, double.PositiveInfinity)]
+    [InlineData(double.NegativeInfinity, 1, 1)]
+    [InlineData(1, double.NegativeInfinity, 1)]
+    [InlineData(1, 1, double.NegativeInfinity)]
+    [InlineData(double.NaN, 1, 1)]
+    [InlineData(1, double.NaN, 1)]
+    [InlineData(1, 1, double.NaN)]
+    public void TestException(double a, double b, double c)
+    {
+        //var squareEquation = new SquareEquation();
+        var argExc = new ArgumentException();
+        try
+        {
+            var roots = SquareEquation.Solve(a, b, c);
+        }
+        catch (Exception exc)
+        {
+            Assert.Equal(exc.GetType(), argExc.GetType());
+        }
+    }
 
-[InlineData(double.NaN, 1, 1)]
-[InlineData(1, double.NaN, 1)]
-[InlineData(1, 1, double.NaN)]
+    [Theory]
+    [InlineData(1, 2, 1)]
+    [InlineData(4, 8, 4)]
+    public void TestOneRoot(double a, double b, double c)
+    {
+        //var squareEquation = new SquareEquation();
+        double[] roots = SquareEquation.Solve(a, b, c);
 
-[InlineData(double.NegativeInfinity, 1, 1)]
-[InlineData(1, double.NegativeInfinity, 1)]
-[InlineData(1, 1, double.NegativeInfinity)]
+        bool rightSolution = true;
+        double eps = 1e-9;
 
-[InlineData(double.PositiveInfinity, 1, 1)]
-[InlineData(1, double.PositiveInfinity, 1)]
-[InlineData(1, 1, double.PositiveInfinity)]
-public void Test_for_Exception(double a, double b, double c)
-{
-var SquareEquation = new SquareEquation();
-var argExc= new ArgumentException();
+        foreach (double i in roots)
+        {
+            if (Math.Abs(a * Math.Pow(i, 2) + b * i + c) > eps)
+            {
+                rightSolution = false;
+            }
+        }
+        Assert.True(rightSolution);
+    }
 
-try
-{
-var result = SquareEquation.Solve(a,b,c);
-}
-catch (Exception ex)
-{
-Assert.Equal(ex.GetType(), argExc.GetType());
-}
-}
+    [Theory]
+    [InlineData(1, 72, 1)]
+    [InlineData(4, 88, 4)]
+    public void TestTwoRoots(double a, double b, double c)
+    {
+        //var SquareEquation = new SquareEquation();
+        double[] roots = SquareEquation.Solve(a, b, c);
+        bool rightSolution = true;
+        double eps = 1e-9;
+        foreach (double i in roots)
+        {
+            if (Math.Abs(a * Math.Pow(i, 2) + b * i + c) > eps)
+            {
+                rightSolution = false;
+            }
+        }
+        Assert.True(rightSolution);
 
-[Theory]
+    }
 
-[InlineData(1, 2, 1)]
-[InlineData(4, 8, 4)]
-
-public void Test_for_one_root(double a, double b, double c)
-{
-var SquareEquation = new SquareEquation();
-double[] result = SquareEquation.Solve(a,b,c);
-
-bool rightSolution = true;
-double eps = 1e-9;
-
-foreach(var i in result)
-{
-if(Math.Abs(a*Math.Pow(i,2)+b*i+c)>eps)
-{
-rightSolution=false;
-}
-}
-Assert.True(rightSolution);
-}
-
-[Theory]
-
-[InlineData(1, 72, 1)]
-[InlineData(4, 88, 4)]
-
-public void Test_for_two_roots(double a, double b, double c)
-{
-var SquareEquation = new SquareEquation();
-
-double[] result = SquareEquation.Solve(a,b,c);
-
-bool rightSolution = true;
-double eps = 1e-9;
-
-foreach(var i in result)
-{
-if(Math.Abs(a*Math.Pow(i,2)+b*i+c)>eps)
-{
-rightSolution=false;
-}
-}
-Assert.True(rightSolution);
-
-}
-
-[Theory]
-
-[InlineData(10000, 1, 1)]
-[InlineData(1, 1, 24)]
-
-public void Test_for_no_roots(double a, double b, double c)
-{
-var SquareEquation = new SquareEquation();
-
-double[] result = SquareEquation.Solve(a,b,c);
-Assert.Empty(result);
-}
+    [Theory]
+    [InlineData(10000, 1, 1)]
+    [InlineData(1, 1, 24)]
+    public void TestNoRoots(double a, double b, double c)
+    {
+        //var SquareEquation = new SquareEquation();
+        double[] roots = SquareEquation.Solve(a, b, c);
+        Assert.Empty(roots);
+    }
 }

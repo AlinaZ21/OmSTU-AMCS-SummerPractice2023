d1c1c1e5173d05112e522de331ebba01ff910223
AlinaZ <a.n.zadorojnaia@gmail.com>
2023-07-03 13:09:02 +0000

    addsolut

--- spacebattletests/UnitTest1.cs
+++ spacebattletests/UnitTest1.cs
@@ -113,6 +113,16 @@ namespace spacebattletests
             catch { }
         }
 
+        [When(@"происходит вращение вокруг собственной оси")]
+        public void происходит_вращение_вокруг_собственной_оси()
+        {
+            try
+            {
+                resultRotat = SpaceShip.Rotation(canRotation, knownCorner, knownAngularSpeen, corner, angularSpeed);
+            }
+            catch { }
+        }
+
         [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
         public void космический_корабль_перемещается_в_точку_пространства_с_координатами
         (double x, double y)

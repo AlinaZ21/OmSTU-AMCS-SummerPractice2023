289cf80b19edd028ef131b4a9e11ac919eab2dd0
AlinaZ <a.n.zadorojnaia@gmail.com>
2023-07-03 12:40:24 +0000

    addsol

--- spacebattle/spacebattle.cs
+++ spacebattle/spacebattle.cs
@@ -11,4 +11,23 @@ public class SpaceShip
         double[] result = { position[0] + speed[0], position[1] + speed[1] };
         return result;
     }
+    public static double Rotation(bool canRotation, bool knownCorner,
+    bool knownAngularSpeen, double corner, double angularSpeed)
+    {
+        if (!canRotation || !knownCorner || !knownAngularSpeen)
+        {
+            throw new Exception();
+        }
+        double result = corner + angularSpeed;
+        return result;
+    }
+    public static double Fuel(double fuel, double consumption)
+    {
+        if (fuel - consumption < 0)
+        {
+            throw new Exception();
+        }
+        double result = fuel - consumption;
+        return result;
+    }
 }
--- spacebattletests/UnitTest1.cs
+++ spacebattletests/UnitTest1.cs
@@ -9,14 +9,44 @@ namespace spacebattletests
         private readonly ScenarioContext scenarioContext;
         private double[] position = new double[2];
         private double[] speed = new double[2];
-        private bool canMove = true, knownSpeed = true, knownPosition = true;
-        private double[] result = new double[2];
+        private double corner, angularSpeed, fuel, consumption;
+        private bool canMove = true, canRotation = true, knownSpeed = true, knownPosition = true, knownAngularSpeen = true, knownCorner = true;
+        private double[] resultMove = new double[2];
+        private double resultRotat;
+        private double resultFuel;
 
         public Перемещение_игрового_объекта(ScenarioContext scenario_Context)
         {
             scenarioContext = scenario_Context;
         }
 
+        [Given(@"космический корабль имеет топливо в объеме \((.*)\)")]
+        public void космический_корабль_имеет_топливо_в_объеме
+        (double x)
+        {
+            fuel = x;
+        }
+
+        [Given(@"имеет скорость расхода топлива \((.*)\)")]
+        public void имеет_скорость_расхода_топлива
+        (double x)
+        {
+            consumption = x;
+        }
+
+        [Given(@"имеет угол наклона \((.*)\) град к оси Ох")]
+        public void имеет_угол_наклона_к_оси_Ох
+        (double x)
+        {
+            corner = x;
+        }
+
+        [Given(@"имеет мнгновенную угловую скорость \((.*)\) град")]
+        public void имеет_мнгновенную_угловую_скорость
+        (double x)
+        {
+            angularSpeed = x;
+        }
 
         [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
         public void космический_корабль_находится_в_точке_пространства_с_координатами
@@ -41,13 +71,23 @@ namespace spacebattletests
             knownPosition = false;
         }
 
-
         [Given(@"скорость корабля определить невозможно")]
         public void скорость_корабля_определить_невозможно()
         {
             knownSpeed = false;
         }
 
+        [Given(@"угловую скорость корабля определить невозможно")]
+        public void угловую_скорость_корабля_определить_невозможно()
+        {
+            knownAngularSpeen = false;
+        }
+
+        [Given(@"угол наклона корабля к оси Ох определить невозможно")]
+        public void угол_наклона_корабля_определить_невозможно()
+        {
+            knownCorner = false;
+        }
 
         [Given(@"изменить положение в пространстве космического корабля невозможно")]
         public void изменить_положение_в_пространстве_космического_корабля_невозможно()
@@ -55,25 +95,46 @@ namespace spacebattletests
             canMove = false;
         }
 
+        [Given(@"изменить угол наклона космического корабля невозможно")]
+        public void изменить_кгол_наклона_космического_корабля_невозможно()
+        {
+            canRotation = false;
+        }
 
         [When(@"происходит прямолинейное равномерное движение без деформации")]
         public void происходит_прямолинейное_равномерное_движениебез_деформации()
         {
             try
             {
-                result = SpaceShip.Movement(canMove, knownSpeed,
+                resultMove = SpaceShip.Movement(canMove, knownSpeed,
                 knownPosition, speed, position);
+                resultFuel = SpaceShip.Fuel(fuel, consumption);
             }
             catch { }
         }
 
-
         [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
         public void космический_корабль_перемещается_в_точку_пространства_с_координатами
-        (double p0, double p1)
+        (double x, double y)
         {
-            double[] expected = { p0, p1 };
-            Assert.Equal(expected, result);
+            double[] expected = { x, y };
+            Assert.Equal(expected, resultMove);
+        }
+
+        [Then(@"новый объем топлива космического корабля \((.*)\)")]
+        public void новый_объем_топлива
+        (double x)
+        {
+            double expected = x;
+            Assert.Equal(expected, resultFuel);
+        }
+
+        [Then(@"новый угол наклона космического корабля \((.*)\)")]
+        public void новый_угол_наклона
+        (double x)
+        {
+            double expected = x;
+            Assert.Equal(expected, resultRotat);
         }
 
         [Then(@"возникает ошибка Exception")]
@@ -81,9 +142,9 @@ namespace spacebattletests
         {
             Assert.Throws<Exception>(() => SpaceShip.Movement(canMove, knownSpeed,
             knownPosition, speed, position));
+            Assert.Throws<Exception>(() => SpaceShip.Rotation(canRotation, knownCorner,
+            knownAngularSpeen, corner, angularSpeed));
+            Assert.Throws<Exception>(() => SpaceShip.Fuel(fuel, consumption));
         }
-
-
-
     }
 }

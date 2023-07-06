b3ffb943317a6deda13bcbfbf6e8d19453e43f97
AlinaZ <a.n.zadorojnaia@gmail.com>
2023-07-06 09:55:48 +0000

    debaggerEx

---
+++ Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using SpaceShip;
+
+class poolWork
+{
+    public static void Main()
+    {
+        Pool<Space_ship> pool = new Pool<Space_ship>(50);
+        using (PoolGuard<Space_ship> poolGuard = new PoolGuard<Space_ship>(pool))
+        {
+            Space_ship ship = poolGuard.Get_Object();
+            Console.WriteLine($"{ship.hp}");
+            Console.WriteLine($"{ship.speed}");
+        }
+    }
+}
---
+++ SpaceShip/Class1.cs
@@ -0,0 +1,57 @@
+﻿namespace SpaceShip;
+
+public class Space_Ship
+{
+    public int hp = 1000;
+    public int speed = 300;
+}
+
+
+public class Pool<spacepool> where spacepool : new()
+{
+    Queue<spacepool> SpaceShips = new Queue<spacepool>();
+    int count;
+
+    public Pool(int count)
+    {
+        this.count = count;
+
+        for (int i = 0; i < count; i++)
+        {
+            SpaceShips.Enqueue(new spacepool());
+        }
+    }
+
+    public spacepool Get_Object()
+    {
+        return SpaceShips.Dequeue();
+    }
+
+    public void Release_Object(spacepool ship)
+    {
+        SpaceShips.Enqueue(ship);
+    }
+}
+
+
+
+public class PoolGuard<spacepool> : IDisposable where spacepool : new()
+{
+    Pool<spacepool> pool;
+    spacepool ship;
+    public PoolGuard(Pool<spacepool> pool)
+    {
+        ship = pool.Get_Object();
+        this.pool = pool;
+    }
+
+    public void Dispose()
+    {
+        pool.Release_Object(ship);
+    }
+
+    public spacepool Get_Object()
+    {
+        return ship;
+    }
+}
